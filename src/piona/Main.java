72ec6fdecf0adb25f944cc33d6b71087eea18ba7
Mauricio Sereno <msereno@apoioprodesp.sp.gov.br>
2024-10-03 16:34:10 -0300

    Fix: Ajuste de pipeline

---
+++ src/piona/Main.java
@@ -0,0 +1,17 @@
+package piona;
+
+/**
+ * Main class.
+ */
+public class Main {
+    
+    /**
+     * Application entry point.
+     * 
+     * @param args Application arguments.
+     */
+    public static void main(String[] args) {
+        HelloWorld hw = new HelloWorld();
+        System.out.println(hw);
+    }
+}
